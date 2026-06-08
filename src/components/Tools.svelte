6182fc8db8afd2bed3caf4277d5602dea80bc3ef
Tyler Van Sant <tyler.vansant@indoormedia.com>
2026-06-08 10:18:18 -0700

    fix: update counter sign API tunnel URL

--- src/components/Tools.svelte
+++ src/components/Tools.svelte
@@ -420,7 +420,7 @@ Store: ${store}
   const isDev = window.location.hostname === 'localhost';
   let COUNTER_SIGN_API = isDev 
     ? 'http://localhost:3333'
-    : 'https://stat-abc-willow-brown.trycloudflare.com';
+    : 'https://steps-andreas-dare-using.trycloudflare.com';
   
   // On production, try to fetch the latest tunnel URL
   if (!isDev) {
